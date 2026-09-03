8ee06e0cbbeace1441f7d93fb81f52c237edf078
Luana Gabrielly Pszymus <ens-luanapszymus@ugv.edu.br>
2026-09-03 14:52:44 -0300

    test: verifica falha no pipeline

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,7 +1,7 @@
 const { somar, subtrair } = require("../src/calculadora");
 
 test("deve somar dois números", () => {
-  expect(somar(2, 3)).toBe(5);
+  expect(somar(2, 3)).toBe(6);
 });
 
 test("deve subtrair dois números", () => {
